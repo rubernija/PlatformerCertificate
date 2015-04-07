c1640fe08abce354a6bc0a5913900421556fac75
MarounR <rizk74@hotmail.com>
2015-04-07 13:26:24 +1000

    Keyboard and Layer Script added

---
+++ player.js
@@ -0,0 +1,41 @@
+var player = function () {
+
+	this.image = document.createElement("img");
+	this.x = canvas.width/2;
+	this.y = canvas.height/2;
+	this.width = 159;
+	this.height = 163;
+	
+	this.velocityX = 0;
+	this.velocityY = 0;
+	
+	this.angularVelocity = 0;
+	this.rotation = 0;
+	
+	this.image.src = "hero.png";
+};
+
+player.prototype.update = function(deltatime)
+{
+	if ( typeof(this.rotation) === "undefined" )
+		this.rotation= 0;
+
+		if (keyboard.isKeyDown(keyboard.KEY_SPACE))
+		{
+			this.rotation -= deltaTime;		
+		}
+		else
+		{
+			this.rotation += deltaTime;
+		}
+};
+
+player.prototype.draw = function(context)
+{
+	context.save();
+		context.translate(this.x, this.y);
+		context.rotate(this.rotation);
+		context.drawImage(this.image, -this.width/2, this.height/2);
+	context.restore();
+	
+};
